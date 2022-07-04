a79886b12768947583615cfd1516c7869511dde2
Capwonderbread <hectcast10@yahoo.com>
2022-07-03 23:30:24 -0700

    fixed typo

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,7 +2,7 @@ let db;
 const request = indexedDB.open('financial-tracker', 1);
 
 request.onupgradeneeded = function(event) {
-  const db = envent.atrget.result;
+  const db = event.target.result;
   db.createObjectStore('new_transaction', { autoIncrement: true});
 };
 
